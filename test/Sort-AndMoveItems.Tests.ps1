2ee973cd45bb89475891f1b6b24c62f98c6f6a42
iodar <granzowdario@gmail.com>
2020-05-31 10:54:37 +0200

    test: added function to create testdatet

--- test/Sort-AndMoveItems.Tests.ps1
+++ test/Sort-AndMoveItems.Tests.ps1
@@ -1,9 +1,11 @@
 $scriptFile = "./../src/Sort-AndMoveItems.ps1"
 
-function Assert-HashtableEquality() {
+# CUSTOM ASSERTS
+
+function Assert-EqualHashTable() {
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipeline = 1)]
+        [Parameter()]
         [hashtable]
         $Actual,
         [Parameter()]
@@ -14,11 +16,36 @@ function Assert-HashtableEquality() {
     $Actual.Keys | ForEach-Object { $Actual[$_] | Should -Be $Expected[$_] }
 }
 
+# TEST HELPER FUNCTIONS
+
+function New-Testdata {
+    param (
+        [string] $TargetFolder
+    )
+    
+    New-Item -ItemType Directory -Path $TargetFolder -Name "001.JPEG" &&
+    Set-ItemProperty -Path "$TargetFolder/001.JPEG" -Name LastWriteTime -Value "2019-09-10T16:45:23.763"
+
+    New-Item -ItemType Directory -Path $TargetFolder -Name "002.JPEG" &&
+    Set-ItemProperty -Path "$TargetFolder/002.JPEG" -Name LastWriteTime -Value "2020-01-17T16:45:23.763"
+
+    New-Item -ItemType Directory -Path $TargetFolder -Name "003.JPEG" &&
+    Set-ItemProperty -Path "$TargetFolder/003.JPEG" -Name LastWriteTime -Value "2020-02-21T16:45:23.763"
+
+    New-Item -ItemType Directory -Path $TargetFolder -Name "004.JPEG" &&
+    Set-ItemProperty -Path "$TargetFolder/004.JPEG" -Name LastWriteTime -Value "2020-02-12T16:45:23.763"
+
+    New-Item -ItemType Directory -Path $TargetFolder -Name "005.JPEG" &&
+    Set-ItemProperty -Path "$TargetFolder/005.JPEG" -Name LastWriteTime -Value "2020-04-13T16:45:23.763"
+
+    return @("$TargetFolder/001.JEPG", "$TargetFolder/002.JEPG", "$TargetFolder/003.JEPG", "$TargetFolder/004.JEPG", "$TargetFolder/005.JEPG")
+}
+
 Describe "Sort-AndMoveItems.ps1" {
     Context "import and return" {
         It "should return values of parametes" {
-            & $scriptFile -SourceFolder "./test" -TargetFolder "./test2" -CustomPostfix "NIKON" |
-            Assert-HashtableEquality -Expected @{
+            $scriptReturnValue = & $scriptFile -SourceFolder "./test" -TargetFolder "./test2" -CustomPostfix "NIKON"
+            Assert-EqualHashTable -Actual $scriptReturnValue -Expected @{
                 SourceFolder  = "./test"
                 TargetFolder  = "./test2"
                 CustomPostfix = "NIKON"
